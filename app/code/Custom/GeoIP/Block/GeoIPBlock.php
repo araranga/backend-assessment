a2b29ed9a96d71adf841efe06c3a74890a4c87f6
Ardee <ardee@growth-rocket.com>
2025-01-30 15:12:43 +0800

    Initial Install EX-01-initial-req123

---
+++ app/code/Custom/GeoIP/Block/GeoIPBlock.php
@@ -0,0 +1,25 @@
+<?php
+namespace Custom\GeoIP\Block;
+
+use Magento\Framework\View\Element\Template;
+
+class GeoIPBlock extends Template
+{
+    protected $geoIPService;
+
+    public function __construct(
+        Template\Context $context,
+        \Custom\GeoIP\Model\GeoIPService $geoIPService,
+        array $data = []
+    ) {
+        parent::__construct($context, $data);
+        $this->geoIPService = $geoIPService;
+    }
+
+    public function getCountryCode()
+    {
+        return $this->geoIPService->getCountryCode();
+    }
+
+
+}
